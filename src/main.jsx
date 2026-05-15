2648c320bbe9ee9b3895dc3a4bc367f4d2898fe9
OsirisCapital <148310531+OsirisCapital@users.noreply.github.com>
2026-05-15 22:41:23 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './tba-site-complet.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
